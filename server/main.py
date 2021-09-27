787d8593400073a24dd871504662a59738aecfda
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-09-27 12:36:38 -0500

    Cache añadido y login1

--- server/main.py
+++ server/main.py
@@ -1,6 +1,26 @@
 from flask import Flask 
+from flask import render_template
+import glob, os.path
 
-jkorp = Flask()
+jkorp = Flask(__name__)
+
+#Conseguir cache de paginas estaticas
+def getCache():
+    paths = glob.glob("server/templates/*.html")
+    cacheDict = dict()
+    with jkorp.app_context():
+        for p in paths:
+            base =  os.path.basename(p)
+            cacheDict[base] = render_template(base)
+            render_template(base)
+    return cacheDict
+Cache = getCache()
+
+#Definir login handler
+@jkorp.route("/login")
+def login():
+    return Cache["login.html"]
+
 
 if __name__ == "__main__":
-    jkorp.run()
+    jkorp.run(debug=True)
