1400388d374f68b7cde02f8ecf7d65f9c6365cb2
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-12-02 10:27:49 -0500

    se añade una libreria para conectar con psql (TEMPORAL)

--- app/app/src/main/java/com/app/jkorp/MainActivity.java
+++ app/app/src/main/java/com/app/jkorp/MainActivity.java
@@ -2,12 +2,38 @@ package com.app.jkorp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*String jdbcURL = "jdbc:postgresql://190.236.90.243:5432/jkorp";
+        String user = "postgres";
+        String password = "root";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            Toast.makeText(getApplicationContext(),"Error getting driver", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, user, password);
+            Toast.makeText(getApplicationContext(),"Connected To DB", Toast.LENGTH_SHORT).show();
+
+            connection.close();
+        } catch (SQLException throwables) {
+            Toast.makeText(getApplicationContext(),"Error Connecting To DB", Toast.LENGTH_SHORT).show();
+            throwables.printStackTrace();
+        }*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
